6395ee320490aebf3746ae2413cfb54ef7552a53
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-05-28 16:46:12 +0300

    Update CasbinRole.go

--- config/CasbinRole.go
+++ config/CasbinRole.go
@@ -1,10 +1,11 @@
 package config
 
 import (
+	"os"
 
 	"github.com/casbin/casbin/v2"
-	"os"
 	"github.com/joho/godotenv"
+
 	//database_SQL
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 	//end_database_SQL
@@ -13,16 +14,14 @@ import (
 	//end_mongodb
 )
 
-
-
 func CasbinRole() *casbin.Enforcer {
-	
-		//env
+
+	//env
 	errenv := godotenv.Load()
-	 if errenv != nil {
-	    	panic("Error loading .env file")
-		}
-	
+	if errenv != nil {
+		panic("Error loading .env file")
+	}
+
 	DB_USERNAME := os.Getenv("DB_USERNAME")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_HOST := os.Getenv("DB_HOST")
@@ -30,31 +29,29 @@ func CasbinRole() *casbin.Enforcer {
 	//database_SQL
 	DB_DATABASE := os.Getenv("DB_DATABASE")
 	//end_database_SQL
-	
-	//mysql	
+
+	//mysql
 	a, errcasbindb := gormadapter.NewAdapter("mysql", DB_USERNAME+":"+DB_PASSWORD+"@tcp("+DB_HOST+":"+DB_PORT+")/"+DB_DATABASE, true)
-    //postgres
+	//postgres
 	//a, errcasbindb := gormadapter.NewAdapter("postgres", "host="+DB_HOST+" port="+DB_PORT+" user="+DB_USERNAME+" dbname="+DB_DATABASE+" password="+DB_PASSWORD, true)
-    //sqlite
+	//sqlite
 	//a, errcasbindb := gormadapter.NewAdapter("sqlite3", "/tmp/gorm.db", true)
-    //mssql
+	//mssql
 	//a, errcasbindb := gormadapter.NewAdapter("mssql", "sqlserver://"+DB_USERNAME+":"+DB_PASSWORD+"@"+DB_HOST+":"+DB_PORT+"?database="+DB_DATABASE, true)
 	//mongodb
-    //a, errcasbindb := mongodbadapter.NewAdapter("mongodb://"+DB_USERNAME+":"+DB_PASSWORD+"@"+DB_HOST+":"+DB_PORT)
-
+	//a, errcasbindb := mongodbadapter.NewAdapter("mongodb://"+DB_USERNAME+":"+DB_PASSWORD+"@"+DB_HOST+":"+DB_PORT)
 
 	if errcasbindb != nil {
 		panic("Failed to connect to database!")
-	  }
+	}
 
-	  e, errcasbin := casbin.NewEnforcer("config/Casbin_role_model.conf", a)
+	e, errcasbin := casbin.NewEnforcer("config/Casbin_role_model.conf", a)
 
 	if errcasbin != nil {
 		panic("Failed to casbin!")
-	  }
+	}
 
 	e.LoadPolicy()
-
 
 	return e
 }
