316f79d6947f5e878a3167b3522d31e01894f121
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-05-28 16:39:59 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "os"
-	"github.com/gin-gonic/gin"
-	"larago/config"
 	"larago/app/Http/Controllers"
 	"larago/app/Http/Middleware"
-	"github.com/utrack/gin-csrf"
-	"github.com/joho/godotenv"
+	"larago/config"
+	"os"
+
 	"github.com/gin-contrib/sessions"
-	
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	csrf "github.com/utrack/gin-csrf"
+
 	///sessions_redis
 	//"github.com/gin-contrib/sessions/redis"
 	//end_sessions_redis
@@ -20,17 +21,14 @@ import (
 	//"github.com/bradfitz/gomemcache/memcache"
 	//"github.com/gin-contrib/sessions/memcached"
 	//end_Memcached
-
 )
-
-
 
 func main() {
 
-    //database_SQL
+	//database_SQL
 	config.Init()
 	//end_database_SQL
-	
+
 	//Mongodb
 	//config.Init_Mongo()
 	//end_Mongodb
@@ -40,13 +38,13 @@ func main() {
 	if errenv != nil {
 		panic("Error loading .env file")
 	}
-    //end_env
+	//end_env
 
 	APP_KEYS := os.Getenv("APP_KEYS")
 
 	//gin
 	//switch to "release" mode in production
-	
+
 	//gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
@@ -56,7 +54,7 @@ func main() {
 	//sessions_cookie
 	store := cookie.NewStore([]byte(APP_KEYS))
 	//end_sessions_cookie
-	
+
 	//redis_sessions
 	//REDIS_HOST := os.Getenv("REDIS_HOST")
 	//REDIS_PASSWORD := os.Getenv("REDIS_PASSWORD")
@@ -64,7 +62,7 @@ func main() {
 	//REDIS_SECRET := os.Getenv("REDIS_SECRET")
 
 	//store, err := redis.NewStore(10, "tcp", REDIS_HOST+":"+REDIS_PORT, REDIS_PASSWORD, []byte(REDIS_SECRET))
-		
+
 	//if err != nil {
 	//		panic("Failed to connect to redis_sessions!")
 	//	  }
@@ -74,46 +72,44 @@ func main() {
 	//store := memcached.NewStore(memcache.New("localhost:11211"), "", []byte("APP_KEYS"))
 	//end_Memcached
 
-    //sessions_use
+	//sessions_use
 	r.Use(sessions.Sessions("larago", store))
-    //end_sessions
-    
+	//end_sessions
+
 	//CSRF_Middleware
 	r.Use(csrf.Middleware(csrf.Options{
-        Secret: APP_KEYS,
-        ErrorFunc: func(c *gin.Context) {
-            c.String(400, "CSRF token mismatch")
-            c.Abort()                                                                                                                                                                             
-        },
+		Secret: APP_KEYS,
+		ErrorFunc: func(c *gin.Context) {
+			c.String(400, "CSRF token mismatch")
+			c.Abort()
+		},
 	}))
 	//end_CSRF_Middleware
 
-
-    //gin_html_and_static
+	//gin_html_and_static
 	r.Static("/public", "./public")
 	r.Static("/node_modules", "./node_modules")
 	r.LoadHTMLGlob("resources/views/*.html")
 	//end_gin_html_and_static
 
+	//gin_route_middleware
 
-    //gin_route_middleware
-    
 	r.GET("/", Controllers.GetWelcome)
 
-    auth := r.Group("/auth")
+	auth := r.Group("/auth")
 	Controllers.Auth(auth.Group("/"))
 
 	//Casbin_Role_Middleware
 	//r.Use(Middleware.AuthCasbinMiddleware(true))
-    //end_Casbin_Role_Middleware
+	//end_Casbin_Role_Middleware
 
-    //Auth_Middleware
+	//Auth_Middleware
 	r.Use(Middleware.AuthMiddleware(true))
 	//end_Auth_Middleware
 
 	users := r.Group("/users")
 	Controllers.UsersRegister(users.Group("/"))
-	
+
 	home := r.Group("/home")
 	Controllers.Home(home.Group("/"))
 
@@ -132,7 +128,7 @@ func main() {
 	//})
 	//end_test
 
-    PORT := os.Getenv("PORT")
+	PORT := os.Getenv("PORT")
 	r.Run(PORT) // listen and serve on 0.0.0.0:8080
 }
 
