dc3d57bd401e7a39538b342d5a4234b332d830fe
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-05-28 16:45:35 +0300

    Update UserModel.go

--- app/Model/UserModel.go
+++ app/Model/UserModel.go
@@ -12,15 +12,14 @@ import (
 
 //database_SQL
 type UserModel struct {
-        gorm.Model
-	ID           uint    `gorm:"column:id;primary_key"`
-	Name         string  `gorm:"column:name;unique_index"`
-	Email        string  `gorm:"column:email;unique_index"`
-	Password     string  `gorm:"column:password;not null"`
-//	//Casbinrole
-	Role         string  `gorm:"column:role"`
-
-} 
+	gorm.Model
+	ID       uint   `gorm:"column:id;primary_key"`
+	Name     string `gorm:"column:name;unique_index"`
+	Email    string `gorm:"column:email;unique_index"`
+	Password string `gorm:"column:password;not null"`
+	//	//Casbinrole
+	Role string `gorm:"column:role"`
+}
 
 //func (UserModel) TableName() string {
 //	return "custom_table_users"
@@ -29,15 +28,15 @@ type UserModel struct {
 
 //MongoDB
 //type UserModel struct {
-	//primitive
-    //ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+//primitive
+//ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 //	ID           string  `json:"id" bson:"_id,omitempty"`
 //	Name         string  `json:"name" bson:"name,omitempty"`
 //	Email        string  `json:"email" bson:"email,omitempty"`
 //	Password     string  `json:"password" bson:"password,omitempty"`
-	//Casbinrole
+//Casbinrole
 //	Role         string  `json:"role" bson:"role,omitempty"`
 //	Url         string  `json:"url" bson:"url,omitempty"`
 
-//} 
+//}
 //end MongoDB
