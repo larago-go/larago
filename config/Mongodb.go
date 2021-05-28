583786caf9097331debffb73a3eee0b2b7b23207
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-05-28 16:47:20 +0300

    Update Mongodb.go

--- config/Mongodb.go
+++ config/Mongodb.go
@@ -9,13 +9,14 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"os"
+
 	"github.com/joho/godotenv"
 )
 
 var MongoClient *mongo.Client
 
 func Init_Mongo() {
-  
+
 	errenv := godotenv.Load()
 	if errenv != nil {
 		panic("Error loading .env file")
@@ -25,27 +26,27 @@ func Init_Mongo() {
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
-    
-  uri := "mongodb://"+DB_USERNAME+":"+DB_PASSWORD+"@"+DB_HOST+":"+DB_PORT
-  
-  client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-  if err != nil {
-      log.Fatal(err)
-  }
-  
-  // Create connect
-  err = client.Connect(context.TODO())
-  if err != nil {
-      log.Fatal(err)
-  }
-  
-  // Check the connection
-  err = client.Ping(context.TODO(), nil)
-  if err != nil {
-      log.Fatal(err)
-  }
-  
-  fmt.Println("Connected to MongoDB!")
-    
-    MongoClient = client
+
+	uri := "mongodb://" + DB_USERNAME + ":" + DB_PASSWORD + "@" + DB_HOST + ":" + DB_PORT
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create connect
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	MongoClient = client
 }
