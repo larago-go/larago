6fead50d690b7cd28263c1511d37cff24eda46be
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-05-28 16:45:14 +0300

    Update CasbinRoleModel.go

--- app/Model/CasbinRoleModel.go
+++ app/Model/CasbinRoleModel.go
@@ -12,14 +12,12 @@ import (
 
 //database_SQL
 type CasbinRoleModel struct {
-        gorm.Model
-	ID       uint    `gorm:"column:id;primary_key"`
-	RoleName string  `gorm:"column:v0;unique_index"`
-	Path     string  `gorm:"column:v1"`
-	Method   string  `gorm:"column:v2"`
-
-} 
-
+	gorm.Model
+	ID       uint   `gorm:"column:id;primary_key"`
+	RoleName string `gorm:"column:v0;unique_index"`
+	Path     string `gorm:"column:v1"`
+	Method   string `gorm:"column:v2"`
+}
 
 //func (CasbinRoleModel) TableName() string {
 //	return "casbin_rule"
@@ -28,13 +26,13 @@ type CasbinRoleModel struct {
 
 //MongoDB
 //type CasbinRoleModel struct {
-	//primitive
-    //ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+//primitive
+//ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 //	ID           string  `json:"id" bson:"_id,omitempty"`
 //	RoleName     string  `json:"v0" bson:"v0,omitempty"`
 //	Path         string  `json:"v1" bson:"v1,omitempty"`
 //	Method       string  `json:"v2" bson:"v2,omitempty"`
 
-
-//} 
+//}
 //end_MongoDB
+
