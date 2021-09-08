81c9a155dac0a7cae434d732f4d6ba5d47c1b621
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-09-08 15:37:03 +0300

    Update UserModel.go

--- app/Model/UserModel.go
+++ app/Model/UserModel.go
@@ -5,9 +5,6 @@ import (
 	//database_SQL
 	"gorm.io/gorm"
 	//end_database_SQL
-	//MongoDB
-	//"go.mongodb.org/mongo-driver/bson/primitive"
-	//end MongoDB
 )
 
 //database_SQL
@@ -25,18 +22,3 @@ type UserModel struct {
 //	return "custom_table_users"
 // }
 //end_database_SQL
-
-//MongoDB
-//type UserModel struct {
-//primitive
-//ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-//	ID           string  `json:"id" bson:"_id,omitempty"`
-//	Name         string  `json:"name" bson:"name,omitempty"`
-//	Email        string  `json:"email" bson:"email,omitempty"`
-//	Password     string  `json:"password" bson:"password,omitempty"`
-//Casbinrole
-//	Role         string  `json:"role" bson:"role,omitempty"`
-//	Url         string  `json:"url" bson:"url,omitempty"`
-
-//}
-//end MongoDB
