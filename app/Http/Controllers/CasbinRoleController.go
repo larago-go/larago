33be4ad968634d5b8b6466db79c40be56ce94001
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-09-08 15:39:04 +0300

    Update CasbinRoleController.go

--- app/Http/Controllers/CasbinRoleController.go
+++ app/Http/Controllers/CasbinRoleController.go
@@ -8,16 +8,6 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	csrf "github.com/utrack/gin-csrf"
-	//MongoDB
-	//"go.mongodb.org/mongo-driver/bson"
-	//"go.mongodb.org/mongo-driver/mongo"
-	//"go.mongodb.org/mongo-driver/mongo/options"
-	//"time"
-	//"log"
-	//"context"
-	//"os"
-	//"github.com/joho/godotenv"
-	//end MongoDB
 )
 
 type CasbinRoleAddValidation struct {
@@ -29,7 +19,7 @@ type CasbinRoleAddValidation struct {
 func CasbinRole(router *gin.RouterGroup) {
 
 	router.POST("/post_add", AddPostCasbinRole)
-	router.GET("/list/:v0/delete", DeleteCasbinRole)
+	router.GET("/list/:id/delete", DeleteCasbinRole)
 	router.GET("/list", ViewCasbinRole)
 	router.GET("/add", AddCasbinRole)
 
@@ -54,34 +44,6 @@ func AddPostCasbinRole(c *gin.Context) {
 	config.DB.Save(&role)
 	//end_Gorm_SQL
 
-	//MongoDB
-	////env
-	//errenv := godotenv.Load()
-	//  if errenv != nil {
-	//      panic("Error loading .env file")
-	//    }
-	//DB_DATABASE := os.Getenv("DB_DATABASE")
-
-	//collection := config.MongoClient.Database(DB_DATABASE).Collection("casbinrolemodels")
-	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	//defer cancel()
-
-	//_, err_post := collection.InsertOne(ctx, role)
-
-	//if err_post != nil {
-	//  c.JSON(http.StatusBadRequest, gin.H{
-	//  "msg": "A role with the same name already exists",
-	//})
-	//}
-	//opt := options.Index()
-	//opt.SetUnique(true)
-	//index := mongo.IndexModel{Keys: bson.M{"v0": 1}, Options: opt}
-	//if _, err := collection.Indexes().CreateOne(ctx, index); err != nil {
-	//log.Println("Could not create index:", err)
-	//}
-	//end MongoDB
-
 	//c.JSON(http.StatusOK, gin.H{"data": role})
 	c.Redirect(http.StatusFound, "/role/list")
 }
@@ -104,53 +66,6 @@ func ViewCasbinRole(c *gin.Context) {
 	//Gorm_SQL
 	config.DB.Find(&model)
 	//end_Gorm_SQL
-
-	//MongoDB
-	//	filter := bson.M{}
-
-	//	// Here's an array in which you can store the decoded documents
-	//	var model []*Model.CasbinRoleModel
-
-	//	// Passing nil as the filter matches all documents in the collection
-	//	 //env
-	//  errenv := godotenv.Load()
-	//	if errenv != nil {
-	//		panic("Error loading .env file")
-	//	  }
-	//  DB_DATABASE := os.Getenv("DB_DATABASE")
-
-	//  collection := config.MongoClient.Database(DB_DATABASE).Collection("casbinrolemodels")
-
-	//	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	//	defer cancel()
-
-	//	cur, err := collection.Find(ctx, filter)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-
-	//	// Finding multiple documents returns a cursor
-	//	// Iterating through the cursor allows us to decode documents one at a time
-	//	for cur.Next(ctx) {
-
-	//		// create a value into which the single document can be decoded
-	//		var elem Model.CasbinRoleModel
-	//		err := cur.Decode(&elem)
-	//		if err != nil {
-	//			log.Fatal(err)
-	//		}
-
-	//		model = append(model, &elem)
-	//	}
-
-	//	if err := cur.Err(); err != nil {
-	//		log.Fatal(err)
-	//	}
-
-	//	// Close the cursor once finished
-	//	cur.Close(ctx)
-	//end MongoDB
 
 	c.HTML(http.StatusOK, "casbin_role.html", gin.H{"session_id": sessionID, "session_name": sessionName, "list": model})
 }
@@ -177,7 +92,7 @@ func DeleteCasbinRole(c *gin.Context) {
 	var model Model.CasbinRoleModel
 
 	//Gorm_SQL
-	if err := config.DB.Where("v0 = ?", c.Param("v0")).First(&model).Error; err != nil {
+	if err := config.DB.Where("id = ?", c.Param("id")).First(&model).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
@@ -189,44 +104,6 @@ func DeleteCasbinRole(c *gin.Context) {
 	config.DB.Delete(&model)
 	//end_Gorm_SQL
 
-	//MongoDB
-	////env
-	//errenv := godotenv.Load()
-	//if errenv != nil {
-	//    panic("Error loading .env file")
-	//  }
-	//DB_DATABASE := os.Getenv("DB_DATABASE")
-
-	//collection := config.MongoClient.Database(DB_DATABASE).Collection("casbinrolemodels")
-
-	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	//defer cancel()
-
-	//filter := bson.M{"v0": c.Param("v0")}
-
-	//res := collection.FindOne(ctx, filter).Decode(&model)
-
-	//if res != nil {
-	//  c.JSON(http.StatusBadRequest, gin.H{
-	//   "msg": "err collections find one",
-	// })
-	//}
-
-	//e := config.CasbinRole()
-
-	//e.RemovePolicy(model.RoleName, model.Path, model.Method)
-
-	//_, err := collection.DeleteMany(ctx, filter)
-
-	//if err != nil {
-	//  c.JSON(http.StatusBadRequest, gin.H{
-	//    "msg": "err collections find one",
-	// })
-	//}
-	//end MongoDB
-
 	//c.JSON(http.StatusOK, gin.H{"data": true})
 	c.Redirect(http.StatusFound, "/role/list")
 }
-
