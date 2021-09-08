693413ae0f49e03d88d9fbdd05cf57e9e00fefa0
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-09-08 15:36:37 +0300

    Update CasbinRoleModel.go

--- app/Model/CasbinRoleModel.go
+++ app/Model/CasbinRoleModel.go
@@ -5,9 +5,6 @@ import (
 	//database_SQL
 	"gorm.io/gorm"
 	//end_database_SQL
-	//MongoDB
-	//"go.mongodb.org/mongo-driver/bson/primitive"
-	//end MongoDB
 )
 
 //database_SQL
@@ -23,16 +20,4 @@ type CasbinRoleModel struct {
 //	return "casbin_user_role"
 // }
 //end_database_SQL
-
-//MongoDB
-//type CasbinRoleModel struct {
-//primitive
-//ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-//	ID           string  `json:"id" bson:"_id,omitempty"`
-//	RoleName     string  `json:"v0" bson:"v0,omitempty"`
-//	Path         string  `json:"v1" bson:"v1,omitempty"`
-//	Method       string  `json:"v2" bson:"v2,omitempty"`
-
-//}
-//end_MongoDB
 
