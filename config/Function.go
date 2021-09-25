f1c8ba67d5ec2ba37ff28d1ce395df39c5d90e7a
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2021-09-26 01:22:14 +0300

    Create Function.go

---
+++ config/Function.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomString(n int) string {
+
+	rand.Seed(time.Now().UnixNano())
+
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
