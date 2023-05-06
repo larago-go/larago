278d00725c1bac6317cf576ced460cb5412f1686
alex <guzhavin.aleksey@gmail.com>
2023-05-06 21:00:35 +0300

    update

--- resources/js/Admin/Views/Casbinrole/CasbinroleAdd.vue
+++ resources/js/Admin/Views/Casbinrole/CasbinroleAdd.vue
@@ -97,7 +97,7 @@ const submit = () => {
                 </div>
 
                 <div class="col-span-6 sm:col-span-3">
-                  <label for="last-name" class="block text-sm font-medium text-gray-700">Path</label>
+                  <label for="last-name" class="block text-sm font-medium text-gray-700">Path (Example: /*)</label>
                   <input type="text" name="last-Path" id="last-Path" v-model="datavw.form.path"  autocomplete="Path" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                 </div>
 
